67c2e3d0e2eae81fc97690e0d6bb428e6b6f1c59
fzc <fzc@yyft.tk>
2020-05-28 23:57:29 +0800

    test

---
+++ src/main/java/com/yyft/blog/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.yyft.blog.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/{name}")
+    @ResponseBody
+    public String getUser(@PathVariable String name) {
+        return "hello," + name;
+    }
+}
