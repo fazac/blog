e878b1b2e82a66fc344426a90a4138fcea13dd01
fzc <fzc@yyft.tk>
2020-06-30 12:04:42 +0800

    pom modify

--- src/main/java/com/yyft/blog/controller/YfUserController.java
+++ src/main/java/com/yyft/blog/controller/YfUserController.java
@@ -1,38 +1,57 @@
 package com.yyft.blog.controller;
 
-import com.yyft.blog.entity.YfUsr;
 import com.yyft.blog.service.YfUserService;
 import com.yyft.common.model.Result;
-import lombok.NonNull;
+import com.yyft.common.model.ResultCode;
+import com.yyft.common.utils.collection.type.Pair;
+import com.yyft.common.utils.mapper.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Description
  * @Author fzc
- * @Date 2020-06-15 14:51
+ * @Date 2020-06-24 16:52
  * @Version 1.0
  */
 @Slf4j
-@RestController
-@RequestMapping("/usr")
+@Controller
+@RequestMapping(value = "/blog")
 public class YfUserController {
     @Autowired
     private YfUserService yfUserService;
 
-    @PostMapping(value = "register")
-    public Result register(@NonNull @RequestBody YfUsr yfUsr) {
-        boolean res = yfUserService.register(yfUsr);
-        if (res) {
-            return Result.createSuccess();
-        } else {
-            return Result.createError();
-        }
+    @RequestMapping("login")
+    public String login() {
+        return "login";
     }
 
-    @GetMapping(value = "login")
+    @RequestMapping("home")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @RequestMapping("doLogin")
+    @ResponseBody
     public Result login(@RequestParam("mobile") String mobile, @RequestParam("pass") String pass) {
-        return Result.createSuccess("ok", yfUserService.login(mobile, pass));
+        if (StringUtils.isEmpty(mobile) || StringUtils.isEmpty(pass)) {
+            return Result.createBizError(-1, ResultCode.PARAM_IS_INVALID.message());
+        }
+        Pair<String, String> res = yfUserService.login(mobile, pass);
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("res", res);
+        resMap.put("url", "home");
+        return Result.createSuccess("ok", resMap);
     }
 }
---
+++ src/main/java/com/yyft/blog/controller/api/ApiYfUserController.java
@@ -0,0 +1,38 @@
+package com.yyft.blog.controller.api;
+
+import com.yyft.blog.entity.YfUsr;
+import com.yyft.blog.service.YfUserService;
+import com.yyft.common.model.Result;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Description
+ * @Author fzc
+ * @Date 2020-06-15 14:51
+ * @Version 1.0
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/usr")
+public class ApiYfUserController {
+    @Autowired
+    private YfUserService yfUserService;
+
+    @PostMapping(value = "register")
+    public Result register(@NonNull @RequestBody YfUsr yfUsr) {
+        boolean res = yfUserService.register(yfUsr);
+        if (res) {
+            return Result.createSuccess();
+        } else {
+            return Result.createError();
+        }
+    }
+
+    @GetMapping(value = "login")
+    public Result login(@RequestParam("mobile") String mobile, @RequestParam("pass") String pass) {
+        return Result.createSuccess("ok", yfUserService.login(mobile, pass));
+    }
+}
